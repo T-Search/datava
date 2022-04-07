9844711b74b7ab8db115e55998972e749e5bfef6
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-08 00:48:41 +0200

    Add @ActiveProfiles("test")

--- src/test/java/de/tsearch/datava/DatavaApplicationTests.java
+++ src/test/java/de/tsearch/datava/DatavaApplicationTests.java
@@ -2,8 +2,10 @@ package de.tsearch.datava;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DatavaApplicationTests {
 
     @Test
