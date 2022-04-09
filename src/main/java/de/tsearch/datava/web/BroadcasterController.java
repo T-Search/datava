8555ab2ed37a8fc03e50650ad8cce683535da718
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-09 15:05:16 +0200

    Add broadcaster names caching

--- src/main/java/de/tsearch/datava/web/BroadcasterController.java
+++ src/main/java/de/tsearch/datava/web/BroadcasterController.java
@@ -2,6 +2,7 @@ package de.tsearch.datava.web;
 
 import de.tsearch.datava.database.postgres.entity.Broadcaster;
 import de.tsearch.datava.database.postgres.repository.BroadcasterRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class BroadcasterController {
         this.broadcasterRepository = broadcasterRepository;
     }
 
+    @Cacheable("broadcasterNames")
     @GetMapping("all")
     public List<String> getAllBroadcaster() {
         return StreamSupport.stream(broadcasterRepository.findAll().spliterator(), false).map(Broadcaster::getDisplayName).filter(Objects::nonNull).toList();
